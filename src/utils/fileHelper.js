a07fe07458c50132fe7dc639b44dda1893f9d6d7
Coder <3170495286@qq.com>
2023-02-01 14:47:41 +0800

    fix:fileHelper format peomise version

--- src/utils/fileHelper.js
+++ src/utils/fileHelper.js
@@ -1,30 +1,18 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const fileHelper = {
-  readFile: (path, cb) => {
-    fs.readFile(path, { encoding: 'utf8' }, (err, data) => {
-      if (!err) {
-        cb(data);
-      }
-    });
-  },
-  writeFile: (path, content, cb) => {
-    fs.writeFile(path, content, { encoding: 'utf8' }, (err) => {
-      if (!err) {
-        cb();
-      }
-    });
-  },
+  readFile: (path) => fs.readFile(path, { encoding: 'utf8' }),
+  writeFile: (path, content) => fs.writeFile(path, content, { encoding: 'utf8' }),
 };
 
 const testPath = path.join(__dirname, 'helper.js');
 const testWritePath = path.join(__dirname, 'hello.md');
 
-fileHelper.readFile(testPath, (data) => {
+fileHelper.readFile(testPath).then((data) => {
   console.log(data);
 });
 
-fileHelper.writeFile(testWritePath, '## hello world !', () => {
+fileHelper.writeFile(testWritePath, '## hello world !').then(() => {
   console.log('写入成功');
 });
